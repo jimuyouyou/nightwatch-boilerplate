7282d12e045dfe6198e976680bf89a9fdc361b8b
秦夫现 <qin@qinfuxiandeMacBook-Pro.local>
2017-05-29 17:40:37 +0800

    screenshot

--- nw/tests/reseachOnGoogle.test.js
+++ nw/tests/reseachOnGoogle.test.js
@@ -2,6 +2,8 @@ module.exports = {
     'Search on google': (browser) => {
         browser
       .url('http://www.baidu.com')
+      .saveScreenshot('./screenshot/baidu.png')
+      .end();
     
     }
 };
